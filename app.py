8f309d00f9a03ae7db6b90e738b439e54e717058
chingzhangliu <43977867+chingzhangliu@users.noreply.github.com>
2018-12-05 18:53:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,46 @@
 from flask import Flask, request, abort
-
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import *
-
-app = Flask(__name__)
-
-# Channel Access Token
-line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
-# Channel Secret
-handler = WebhookHandler('YOUR_CHANNEL_SECRET')
-
-# 監聽所有來自 /callback 的 Post Request
-@app.route("/callback", methods=['POST'])
-def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-    return 'OK'
-
-# 處理訊息
+ import os
+ import sys
+ from linebot import (
+     LineBotApi, WebhookHandler
+ )
+ from linebot.exceptions import (
+     InvalidSignatureError
+ )
+ from linebot.models import *
+ 
+ app = Flask(__name__)
+ 
+ ACCESS_TOKEN= os.environ['ACCESS_TOKEN']
+ SECRET= os.environ['CHANNEL_SECRET']
+ 
+ # Channel Access Token
+ line_bot_api = LineBotApi(ACCESS_TOKEN)
+ # Channel Secret
+ handler = WebhookHandler(SECRET)
+ 
+ 
+ @app.route("/")
+ def hello_world():
+     return "hello world!"
+ 
+ 
+ # 監聽所有來自 /callback 的 Post Request
+ @app.route("/callback", methods=['POST'])
+ def callback():
+     # get X-Line-Signature header value
+     signature = request.headers['X-Line-Signature']
+     # get request body as text
+     body = request.get_data(as_text=True)
+     app.logger.info("Request body: " + body)
+     # handle webhook body
+     try:
+         handler.handle(body, signature)
+     except InvalidSignatureError:
+         abort(400)
+     return 'OK'
+ 
+ # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = TextSendMessage(text='Nice to meet you!')
@@ -39,8 +49,7 @@ def handle_message(event):
     # message = TextSendMessage(text=event)
     print(event)
     
-
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+app.run(host='0.0.0.0', port=port)
